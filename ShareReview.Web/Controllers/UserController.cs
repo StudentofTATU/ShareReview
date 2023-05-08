fa03314ce4e87795a20ba58ab6dc46b0e520c446
StudentofTATU <musogulomov123@gmail.com>
2023-05-08 07:18:07 +0500

    User Register and Login

--- ShareReview.Web/Controllers/UserController.cs
+++ ShareReview.Web/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ShareReview.Contracts.Users;
 using ShareReview.Services.Interfaces;
 using ShareReview.Web.ViewModels;
@@ -14,9 +15,13 @@ namespace ShareReview.Web.Controllers
             this.userService = userService;
         }
 
-        public IActionResult Index()
+        [Authorize]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            UserDTO userDTO =await userService.GetCurrentUserAsync(HttpContext);
+            UserViewModel userViewModel=new UserViewModel(userDTO);
+            TempData["userName"] = userDTO.Name;
+            return View(userViewModel);
         }
 
         [HttpGet]
@@ -35,6 +40,33 @@ namespace ShareReview.Web.Controllers
             }
 
             return View();  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginViewModel userViewModel) 
+        {
+            if(!ModelState.IsValid) { return View(userViewModel); }
+
+            var result = await userService.LoginAsync(userViewModel.GetUserDTO());
+
+            if (result.StatusCode == 1)
+            {
+                return RedirectToAction("Index","User");
+            }
+            else
+            {
+                TempData["message"] = result.Message;
+                return RedirectToAction(nameof(Login));
+            }
+        }
+
+        public IActionResult Login() { return View(); }
+
+        [Authorize]
+        public async Task<IActionResult> Logout() 
+        { 
+            await userService.LogoutAsync();
+            return RedirectToAction(nameof(Login)); 
         }
     }
 }
---
+++ ShareReview.Web/ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ShareReview.Contracts.Users;
+
+namespace ShareReview.Web.ViewModels
+{
+    public class UserLoginViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("^.{6,}$", ErrorMessage =
+            "Minimum length 6.")]
+        public string Password { get; set; }
+
+        public LoginUserDTO GetUserDTO()
+        {
+            return new LoginUserDTO
+            {
+                Name = this.Name,
+                Password = this.Password
+            };
+        }
+    }
+}
